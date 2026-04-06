5f4938d147e2459b981f532286021c6ae07b0b15
nate <80553619+machinectrl@users.noreply.github.com>
2026-04-05 21:42:29 -0400

    theme

--- init.lua
+++ init.lua
@@ -250,6 +250,12 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
+  'tpope/vim-fugitive',
+  cmd = { 'Git', 'G', 'Gdiffsplit', 'Gread', 'Gwrite', 'Ggrep', 'GMove', 'GDelete', 'GBrowse' },
+  keys = {
+    { '<leader>gs', '<cmd>Git<cr>', desc = 'Git status' },
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
